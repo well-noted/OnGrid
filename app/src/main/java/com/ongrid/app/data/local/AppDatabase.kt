1ae12f11fad05dfa8cfef269a17e5a87954332c4
Thomas E Tuoti <tetuoti@gmail.com>
2026-05-11 10:29:43 -0700

    Add migration

--- app/src/main/java/com/ongrid/app/data/local/AppDatabase.kt
+++ app/src/main/java/com/ongrid/app/data/local/AppDatabase.kt
@@ -5,7 +5,7 @@ import androidx.room.RoomDatabase
 
 @Database(
     entities = [ProjectEntity::class, ConversationEntity::class, MessageEntity::class, SavedServerEntity::class],
-    version = 2,
+    version = 3,
     exportSchema = false
 )
 abstract class AppDatabase : RoomDatabase() {
